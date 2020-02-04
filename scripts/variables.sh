5696276da0e46c3c7691a4f5320853e0ac4e108c
spirillen <44526987+spirillen@users.noreply.github.com>
2020-02-04 16:13:16 +0100

    testing ROOT_DIR

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -3,9 +3,22 @@
 # **********************
 # Setting date variables
 # **********************
+
+# First setup the working dir
+pushd . > /dev/null
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if ([ -h "${SCRIPT_PATH}" ]) then
+  while([ -h "${SCRIPT_PATH}" ]) do cd `dirname "$SCRIPT_PATH"`; SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+fi
+cd `dirname ${SCRIPT_PATH}.` > /dev/null
+SCRIPT_PATH=`pwd`;
+popd  > /dev/null
+
+ROOT_DIR="$(dirname "$SCRIPT_PATH")"
+
 printf "\nSetting Variables\n"
-export script_dir="${TRAVIS_BUILD_DIR}/scripts"
-export testdir="${TRAVIS_BUILD_DIR}/test_data"
+export script_dir="${ROOT_DIR}/scripts"
+export testdir="${ROOT_DIR}/test_data"
 export testfile="${testdir}/rpz.mypdns.cloud.list"
 export whitelist="${testdir}/whitelist.txt"
 export tag=$(date '+%F %X %Z %z')
